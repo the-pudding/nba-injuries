85192e7f3620ab8f8125e30b0c00208f61122e53
polygraph-umg <russell@polygraph.cool>
2023-05-12 12:35:34 -0700

    redoing

---
+++ src/components/Temp.svelte
@@ -0,0 +1,26 @@
+<p class="title">
+						<span>opponents: {asterisks.opponent.toFixed(1)}</span>
+						<span>winner: {asterisks.winner.toFixed(1)}</span>
+						<span>------------</span>
+						<span>all: {asterisks.all.toFixed(1)}</span>
+						<span>others: {asterisks.allNotOpponent.toFixed(1)}</span>
+					</p>
+					
+				</div>
+				<div class="dnp">
+					<SortTable rows={dnp} {columns} {caption} />
+					<ul>
+						{#each dnp as { name, bbrID, headshot }}
+							{@const src = headshot
+								? `assets/headshots/${bbrID}.png`
+								: "assets/headshot-default.png"}
+							<li>
+								<img {src} alt="headshot of {name}" />
+								<p>{name}</p>
+							</li>
+						{/each}
+					</ul>
+				</div>
+				<p class="injured">
+					playoff injury rate: {Math.round(percentInjured * 100)}%
+				</p>
