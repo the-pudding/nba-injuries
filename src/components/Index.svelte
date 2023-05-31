35d57c5a2b060fe38eebc70b9cb160ec9a9680b5
polygraph-umg <russell@polygraph.cool>
2023-05-31 10:45:45 -0700

    method

--- src/components/Index.svelte
+++ src/components/Index.svelte
@@ -1,11 +1,14 @@
 <script>
 	import Champions from "$components/Champions.svelte";
 	import Intro from "$components/Intro.svelte";
+	import Method from "$components/Method.svelte";
 	// import Footer from "$components/Footer.svelte";
 </script>
 
 <Intro />
 
 <Champions />
+
+<Method />
 
 <!-- <Footer /> -->
---
+++ src/components/Method.svelte
@@ -0,0 +1,21 @@
+<script>
+	import { getContext } from "svelte";
+	const copy = getContext("copy");
+</script>
+
+<div id="method">
+	<h4><strong>Data and Methods</strong></h4>
+	{#each copy.method as { value }}
+		<p>{@html value}</p>
+	{/each}
+</div>
+
+<style>
+	#method {
+		max-width: 45rem;
+		margin: 64px auto 64px;
+		background: var(--color-fg);
+		color: var(--color-bg);
+		padding: 16px 32px;
+	}
+</style>
