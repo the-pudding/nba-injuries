5ce90982f8618a0f49773dceb619a3e795ff40eb
polygraph-umg <russell@polygraph.cool>
2023-05-31 07:27:03 -0700

    update github pages

---
+++ docs/_app/immutable/chunks/2.3e6b4e32.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/_page.svelte.d03d06ab.js";export{t as component};
--- docs/_app/immutable/chunks/4.313f4fff.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/demo-fonts-page.svelte.a1de3664.js";export{t as component};
